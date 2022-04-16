4be1ac04c19f29dd237f58153f4466f372bc8f9d
Pittersss <pedromalaquias608@gmail.com>
2022-04-16 17:56:36 -0300

    Atualização em python

---
+++ datasheets.py
@@ -0,0 +1,30 @@
+#Ficha para listar pessoas
+from asyncio.windows_events import NULL
+import os
+
+fileName = input("Diga o nome do arquivo que deseja criar: \n") + ".txt"
+arquivo = open(fileName, 'w+')
+texto = arquivo.readlines()
+if texto == NULL:
+ newName = input("Qual é o seu nome: \n")
+ print("\n")
+ 
+ texto.append(newName)
+ arquivo.writelines("Nome: " + newName + "\n")
+ 
+ newAge = input("Qual é a sua idade: \n")
+ print("\n")
+ texto.append(str(newAge))
+ arquivo.writelines("Idade: " + newAge + "\n")
+
+ newID = input("Qual é o seu CPF: \n")
+ print("\n")
+ texto.append(str(newID))
+ arquivo.writelines("CPF: " + newID + "\n")
+else:
+    print("Essa ficha já foi preenchida!")
+arquivo.close()
+try:
+  os.replace(fileName, "C:\\Users\\Pedro\\Desktop\\SQL TEST\\" + fileName)
+except PermissionError:
+  print("Você não tem permissão para fazer isso!")    
